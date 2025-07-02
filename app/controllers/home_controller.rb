57623983bf6eb7a12072cf1a2697d0e6116d2478
Rejka Permana <rejkamuhammad@gmail.com>
2025-07-03 06:18:39 +0700

    update design

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     @recent_recipes = Recipe.order(created_at: :desc).limit(6)
     @recent_products = Product.order(created_at: :desc).limit(6)
+    @recent_packagings = Packaging.order(created_at: :desc).limit(6)
     @recent_simulations = MarketSimulation.order(created_at: :desc).limit(3)
   end
 end
